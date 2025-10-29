8c00655bf3dbd20a2c0805f7076cce7a9325ce7a
Vaibhav Shukla <vaibhavshukla.contact@gmail.com>
2025-10-29 09:31:51 +0530

    test case

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dlclark/regexp2 v1.11.5 // indirect
 	github.com/fatih/color v1.18.0 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
@@ -29,6 +30,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sagikazarmark/locafero v0.11.0 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
@@ -36,6 +38,8 @@ require (
 	github.com/spf13/afero v1.15.0 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
@@ -46,4 +50,5 @@ require (
 	golang.org/x/sys v0.36.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/service/vm_service_test.go
@@ -0,0 +1,172 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"vm/internal/modals"
+	"vm/internal/repo/mocks"
+	"vm/pkg/constants"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+// MockLogger is a mock implementation of the cinterface.Logger for testing.
+type MockLogger struct {
+	mock.Mock
+}
+
+func (m *MockLogger) Init() {
+	m.Called()
+}
+
+func (m *MockLogger) Debug(cat constants.Category, sub constants.SubCategory, msg string, extra map[constants.ExtraKey]interface{}) {
+	m.Called(cat, sub, msg, extra)
+}
+
+func (m *MockLogger) Debugf(templateName string, args ...interface{}) {
+	m.Called(templateName, args)
+}
+
+func (m *MockLogger) Info(cat constants.Category, sub constants.SubCategory, msg string, extra map[constants.ExtraKey]interface{}) {
+	m.Called(cat, sub, msg, extra)
+}
+
+func (m *MockLogger) Infof(templateName string, args ...interface{}) {
+	m.Called(templateName, args)
+}
+
+func (m *MockLogger) Warn(cat constants.Category, sub constants.SubCategory, msg string, extra map[constants.ExtraKey]interface{}) {
+	m.Called(cat, sub, msg, extra)
+}
+
+func (m *MockLogger) Warnf(templateName string, args ...interface{}) {
+	m.Called(templateName, args)
+}
+
+func (m *MockLogger) Error(cat constants.Category, sub constants.SubCategory, msg string, extra map[constants.ExtraKey]interface{}) {
+	m.Called(cat, sub, msg, extra)
+}
+
+func (m *MockLogger) Errorf(templateName string, args ...interface{}) {
+	m.Called(templateName, args)
+}
+
+func (m *MockLogger) Fatal(cat constants.Category, sub constants.SubCategory, msg string, extra map[constants.ExtraKey]interface{}) {
+	m.Called(cat, sub, msg, extra)
+}
+
+func (m *MockLogger) Fatalf(templateName string, args ...interface{}) {
+	m.Called(templateName, args)
+}
+
+func TestCreateVMRequest_Hybrid(t *testing.T) {
+	metadata := `{
+        "imageSource": {"imageId": "some-image-id"},
+        "destination": {"hostId":"some-host-id","clusterId":"some-cluster-id"},
+        "vmConfig":{"name":"test-vm","numberOfVms":1,"acceptEula":true},
+        "storageConfig":{"defaultDatastoreId":"datastore-1"}
+    }`
+
+	// --- Unique test case kept separate ---
+	t.Run("Invalid Metadata", func(t *testing.T) {
+		mockRepo := new(mocks.VMRepository)
+		mockLogger := new(MockLogger)
+		vmService := NewVMService(mockRepo, mockLogger)
+
+		mockLogger.On("Info", mock.Anything, mock.Anything, mock.Anything, mock.Anything)
+		mockLogger.On("Error", mock.Anything, mock.Anything, mock.Anything, mock.Anything)
+
+		invalidMetadata := `{"invalid-json": }`
+		vmRequest, err := vmService.CreateVMRequest(context.Background(), constants.VMDeploy, constants.StatusPending, invalidMetadata)
+
+		assert.Nil(t, vmRequest)
+		assert.Error(t, err)
+	})
+
+	// --- Table-driven for cases with same flow ---
+	tests := []struct {
+		name           string
+		setupMocks     func(mockRepo *mocks.VMRepository, mockLogger *MockLogger) string
+		expectError    bool
+		expectVMRequest bool
+	}{
+		{
+			name: "Success",
+			setupMocks: func(mockRepo *mocks.VMRepository, mockLogger *MockLogger) string {
+				expectedID := uuid.New().String()
+				mockLogger.On("Info", mock.Anything, mock.Anything, mock.Anything, mock.Anything)
+
+				mockRepo.On("CreateVMRequest", mock.Anything, mock.AnythingOfType("*modals.VMRequest")).
+					Run(func(args mock.Arguments) {
+						arg := args.Get(1).(*modals.VMRequest)
+						arg.RequestID = expectedID
+					}).Return(nil)
+
+				mockRepo.On("CreateVMDeployInstances", mock.Anything, expectedID, "test-vm", 1).Return(nil)
+				return expectedID
+			},
+			expectError: false,
+			expectVMRequest: true,
+		},
+		{
+			name: "DB Error on CreateVMRequest",
+			setupMocks: func(mockRepo *mocks.VMRepository, mockLogger *MockLogger) string {
+				mockLogger.On("Info", mock.Anything, mock.Anything, mock.Anything, mock.Anything)
+				mockLogger.On("Error", mock.Anything, mock.Anything, mock.Anything, mock.Anything)
+
+				mockRepo.On("CreateVMRequest", mock.Anything, mock.AnythingOfType("*modals.VMRequest")).
+					Return(errors.New("DB insert failed"))
+				return ""
+			},
+			expectError: true,
+			expectVMRequest: false,
+		},
+		{
+			name: "DB Error on CreateVMDeployInstances",
+			setupMocks: func(mockRepo *mocks.VMRepository, mockLogger *MockLogger) string {
+				expectedID := uuid.New().String()
+				mockLogger.On("Info", mock.Anything, mock.Anything, mock.Anything, mock.Anything)
+				mockLogger.On("Error", mock.Anything, mock.Anything, mock.Anything, mock.Anything)
+
+				mockRepo.On("CreateVMRequest", mock.Anything, mock.AnythingOfType("*modals.VMRequest")).
+					Run(func(args mock.Arguments) {
+						arg := args.Get(1).(*modals.VMRequest)
+						arg.RequestID = expectedID
+					}).Return(nil)
+
+				mockRepo.On("CreateVMDeployInstances", mock.Anything, expectedID, "test-vm", 1).
+					Return(errors.New("DB insert failed"))
+
+				return expectedID
+			},
+			expectError: true,
+			expectVMRequest: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRepo := new(mocks.VMRepository)
+			mockLogger := new(MockLogger)
+			vmService := NewVMService(mockRepo, mockLogger)
+
+			tt.setupMocks(mockRepo, mockLogger)
+
+			vmRequest, err := vmService.CreateVMRequest(context.Background(), constants.VMDeploy, constants.StatusPending, metadata)
+
+			if tt.expectVMRequest {
+				assert.NotNil(t, vmRequest)
+				assert.NoError(t, err)
+			} else {
+				assert.Nil(t, vmRequest)
+				assert.Error(t, err)
+			}
+
+			mockRepo.AssertExpectations(t)
+			mockLogger.AssertExpectations(t)
+		})
+	}
+}
